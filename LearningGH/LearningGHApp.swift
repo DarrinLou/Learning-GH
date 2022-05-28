fdadd542a9d281e0152e40a4b9ef506ec3feb1aa
Darrin Lou <darrinpylou123@gmail.com>
2022-05-28 13:34:50 +0800

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Darrin Lou on 28/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
